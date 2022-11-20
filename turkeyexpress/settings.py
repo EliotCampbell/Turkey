b429107bc57482bf4f83f79fbd8a5d7ed340673b
EliotCampbell <geradmitruk@gmail.com>
2022-11-20 19:21:17 +0300

    V1.2

--- turkeyexpress/settings.py
+++ turkeyexpress/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     BASE_DIR.joinpath('static')
 ]
 
+STATIC_ROOT = 'Turkey/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
